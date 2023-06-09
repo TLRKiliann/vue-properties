705b59f04c82d48aa28a6360e3c8e85db0722d62
TLRKiliann <philogenie@protonmail.com>
2023-06-09 12:03:53 +0200

    setup done

---
+++ vue-project/src/components/FirstComp.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "FirstComp",
+  props: ['name'],
+  data() {
+    return {
+      className: 'className'
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="div--h2">
+    <h2 v-bind:class="className">Name : {{ name }}</h2>
+  </div>
+</template>
+
+<style scoped>
+.className {
+    color: lightgreen;
+  }
+</style>
